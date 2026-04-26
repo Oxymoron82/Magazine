5cc4ca7387741772d0c12c65a239d37595ce6b54
Oxymoron82 <oxumoron82@gmail.com>
2026-04-26 17:57:54 +0300

    remove redirect loop

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
+    // Жёстко задаём корень проекта, чтобы Next не путался из-за lock-файлов в home
     root: __dirname,
   },
 
   allowedDevOrigins: ["192.168.100.20"],
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.theissue.xyz",
-          },
-        ],
-        destination: "https://theissue.xyz/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
